b745d39f6c0308ebd46015504ed3095d444b958e
Juancar1682 <96840752+Juancar1682@users.noreply.github.com>
2025-08-30 15:14:33 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
+let count = 0;
+let scoreNow = document.getElementById("score");
+let scoreNow2 = document.getElementById("score-2");
 
-let count = 0
-let scoreNow = document.getElementById("score")
+function add1_2() {
+  count += 1;
+  scoreNow2.textContent = count;
+}
 
+function add2_2() {
+  count += 2;
+  scoreNow2.textContent = count;
+}
+
+function add3_2() {
+  count += 3;
+  scoreNow2.textContent = count;
+}
 function add1() {
-    count += 1
-    scoreNow.textContent = count
-    
+  count += 1;
+  scoreNow.textContent = count;
 }
 
 function add2() {
-    count += 2
-    scoreNow.textContent = count
-    
+  count += 2;
+  scoreNow.textContent = count;
 }
 
 function add3() {
-    count += 3
-    scoreNow.textContent = count
-    
+  count += 3;
+  scoreNow.textContent = count;
 }
